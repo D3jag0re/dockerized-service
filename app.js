20b6dc0e7e756475ed27e4afff62c5f5505d1cb8
Mat B <mbrinkwo@gmail.com>
2024-12-06 15:25:55 -0500

    .env working

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
 // Using express framework 
 const express = require('express');
+const auth = require('basic-auth');
+const env = require('dotenv');
 const app = express();
 const port = 4000;
+require('dotenv').config()
 
+
+// Routes 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello, World!');
 });
 
 app.get('/about', (req, res) => {
     res.send('Welcome to about us page');
 });
 
-app.get('/contact', (req, res) => {
-    res.send('Welcome to contact us page');
+app.get('/secret', (req, res) => {
+    res.send(process.env.SECRET_MESSAGE);
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${port}`);
 });
